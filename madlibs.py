64a27af8c4c3181d2e78a44312c2627287442c77
Md. Shahjalal Shadhin <145322094+ShahjalalShadhin@users.noreply.github.com>
2024-11-01 21:07:13 +0600

    Add files via upload
    Madlibs word game

---
+++ madlibs.py
@@ -0,0 +1,12 @@
+# Madlibs game (Word game where you create a story by fill in blanks with random words.)
+
+adj1 = input("Enter adj1: ")
+noun1 = input("Enter noun1: ")
+adj2 = input("Enter adj2: ")
+verb1 = input("Enter verb1: ")
+adj3 = input("Enter adj3: ")
+
+print(f"Today I went to a {adj1} place.")
+print(f"There I saw a {noun1}.")
+print(f"The {noun1} was {adj2} and {verb1}.")
+print(f"I was {adj3}.")
